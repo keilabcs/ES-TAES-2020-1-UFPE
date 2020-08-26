4b48103f4619dcc881a616ce046c59195a0c01e6
KEILA BARBOSA COSTA <kbcs2@cin.ufpe.br>
2020-08-26 04:12:48 -0300

    update

--- Code/Script.R
+++ Code/Script.R
@@ -5,11 +5,37 @@ library(rstatix)
 library(readxl)
 
 #Read Data
-Survey<- read_excel("~/ES-TAES-2020-1/Base/new_results2.xlsx")  # Carregamento do Arquivo xlsx
-glimpse(Survey)                                                 # Visualização de um resumo dos dados
+Survey<- read_excel("~/ES-TAES-2020-1/Base/rank.xlsx")  # Carregamento do Arquivo xlsx
+glimpse(Survey)                                         # Visualização de um resumo dos dados
 head(Survey)
 
+#Filtrando por categoria
+Build1 <- Survey %>%
+  select(waste, rank, candidate) %>%
+  filter(waste %in% c('build', 'mismanaging'))
+head(Build1)
+
+Build2 <- Survey %>%
+  select(waste, rank, candidate) %>%
+  filter(waste %in% c('build', 'rework'))
+
+Build3 <- Survey %>%
+  select(waste, rank, candidate) %>%
+  filter(waste %in% c('build', 'unnecessarily'))
+
+
 # Realização do teste de Mann-Whitney
-wilcox.test(build ~ candidate, data = Survey)
-wilcox.test(mismanaging ~ candidate, data = Survey)
-wilcox.test(rework ~ candidate, data = Survey)
+wilcox.test(rank ~ waste, data = Build1)
+wilcox.test(rank ~ waste, data = Build2)
+wilcox.test(rank ~ waste, data = Build3)
+
+#Análise descritiva dos dados
+dados %>% group_by(rank) %>% 
+  get_summary_stats(rank, rank, rank, type = "median_iqr")
+
+# Visualização da distribuição
+par(mfrow=c(1,2))
+hist(rank[Build1$waste == "build"],
+     ylab="Frequência", xlab="Rank", main="Grupo Build")
+hist(rank[Build1$waste == "mismanaging"],
+     ylab="Frequência", xlab="Rank", main="Grupo Mismanaging")
